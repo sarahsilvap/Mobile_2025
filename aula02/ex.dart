e350b4b537b8f589b2db8f97d8e8b866c9812623
sarahsilvap <sarahs-pereira@outlook.com>
2025-02-03 20:10:43 -0300

    ex1

---
+++ aula02/ex.dart
@@ -0,0 +1,9 @@
+//Importando a biblioteca dart:io
+import 'dart:io';
+
+//Função principal do código
+void main() [
+  print("Digite seu nome:");
+  //Só irá converter o dado se a entrada não for fazia
+  String nome = stdin.readLineSync()!;
+]
---
+++ aula02/ex1.dart
@@ -0,0 +1,25 @@
+//Importando a biblioteca Dart
+import 'dart:io';
+
+//Função principal do código
+
+void main() {
+  //Declarando a variável do tipo inteira
+  // Chamada idade
+  int idade = 22;
+  print("idade: $idade");
+
+  // Declarando variável do tipo duble
+  double raio = 10.25;
+  print('Raio: $raio');
+
+  // Declarando variável do tipo string
+  String nome = "Sarah";
+  print("Nome $nome");
+  print("Olá $nome! Seja bem-vindo!");
+
+  // Declarando variável do tipo boolean
+  bool st = true;
+  print("A variável st é $st");
+
+}
